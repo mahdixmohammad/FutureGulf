4839e480d3d0931bb5ad58880c24d5b666db0c67
mahdixmohammad <mahdi3mohammad@gmail.com>
2024-06-05 23:59:26 -0400

    Added language selection in navigation bar.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-// when user scrolls down
+// choose language arrow drop down
+const arrow = document.querySelector(".arrow");
+const dropdown = document.querySelector(".dropdown");
+
+arrow.addEventListener("click", () => {
+    arrow.classList.toggle("arrow-active");
+    dropdown.classList.toggle("dropdown-active");
+});
+
+// user scrolls down
 window.addEventListener("scroll", function() {
     let nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
 })
 
-// when user clicks on the hamburger icon (small devices only)
+// user clicks on the hamburger icon (small devices only)
 hamburger = document.querySelector(".hamburger");
 hamburger.onclick = function() {
     let nav = document.querySelector("nav");
